f121e14b3eedb69065d72beac19be6d92508ba7d
birshert <323d5e372407cadc70e39e8ca70df077>
2021-09-06 12:46:10 +0300

    Add util files

---
+++ utils/colab_setup.py
@@ -0,0 +1,42 @@
+import os
+from abc import ABC
+from abc import abstractmethod
+
+
+def download_github_code(path):
+    filename = path.rsplit('/')[-1]
+    os.system('shred -u {}'.format(filename))
+    os.system('wget -q https://github.com/hse-cs-ami/coursera-intro-dl/blob/main/{} -O {}'.format(path, filename))  # TODO: make actual link
+
+
+def download_github_release(path):
+    filename = path.rsplit('/')[-1]
+    os.system('shred -u {}'.format(filename))
+    os.system('wget -q https://github.com/hse-cs-ami/coursera-intro-dl/releases/download/{} -O {}'.format(path, filename))  # TODO: make actual link
+
+
+class WeekSetup(ABC):
+
+    def __init__(self):
+        download_github_code('utils/grading.py')  # TODO: make actual grading file
+
+    @abstractmethod
+    def setup(self):
+        pass
+
+
+class Week02CNN1(WeekSetup):
+
+    def setup(self):
+        pass
+
+
+class Week03CNN2(WeekSetup):
+
+    def __init__(self):
+        super().__init__()
+        self.dataset_name = 'birbs.tar.gz'
+
+    def setup(self):
+        download_github_release('dataset/' + self.dataset_name)
+        os.system(f'tar -xf {self.dataset_name}')
