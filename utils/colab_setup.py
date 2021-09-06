413b25b08ed9baaa3e4f339ca916ce3fff8c0969
birshert <323d5e372407cadc70e39e8ca70df077>
2021-09-06 18:00:32 +0300

    Update

--- utils/colab_setup.py
+++ utils/colab_setup.py
@@ -6,13 +6,15 @@ from abc import abstractmethod
 def download_github_code(path):
     filename = path.rsplit('/')[-1]
     os.system('shred -u {}'.format(filename))
-    os.system('wget -q https://raw.githubusercontent.com/hse-cs-ami/coursera-intro-dl/main/{} -O {}'.format(path, filename))
+    os.system(
+        'wget -q https://raw.githubusercontent.com/hse-cs-ami/coursera-intro-dl/main/{} -O {}'.format(path, filename))
 
 
 def download_github_release(path):
     filename = path.rsplit('/')[-1]
     os.system('shred -u {}'.format(filename))
-    os.system('wget -q https://github.com/hse-cs-ami/coursera-intro-dl/releases/download/{} -O {}'.format(path, filename))
+    os.system(
+        'wget -q https://github.com/hse-cs-ami/coursera-intro-dl/releases/download/{} -O {}'.format(path, filename))
 
 
 class WeekSetup(ABC):
